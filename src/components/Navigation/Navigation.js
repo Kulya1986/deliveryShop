a4512522ac218bc911ea9cd4a6acd892c42613fd
Kulya1986 <87373027+Kulya1986@users.noreply.github.com>
2024-04-23 13:34:49 +0300

    Layout changes(heights adjustment)

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -9,35 +9,37 @@ import React from 'react';
 const Navigation = ({onNavigationTabClick}) => {
 
     return (
-    <Navbar style={{backgroundColor: '#baccaa', color: '#01233e', width:'100%'}}>
-        <Navbar.Group align={Alignment.LEFT}>
-        <Navbar.Heading className='nav-text-style'>
-            <div style={{display:'flex', flexWrap:'nowrap', alignItems:'center'}}>
-                <span>DeliveryShop</span>
-                <img src='/images/delivery.png' alt='' style={{width:'36px'}}/>
-            </div>    
-        </Navbar.Heading>
-        <Navbar.Divider />
-        <Button 
-            className="bp4-minimal nav-text-style" 
-            text="Shop" 
-            style={{color: '#01233e', padding:'0 1.6rem'}}
-            onClick={()=>{onNavigationTabClick('shop')}}
-        />
-        <Button 
-            className="bp4-minimal nav-text-style" 
-            text="Shopping Cart" 
-            style={{color: '#01233e', padding:'0 1.6rem'}}
-            onClick={()=>{onNavigationTabClick('cart')}}
-        />
-         <Button 
-            className="bp4-minimal nav-text-style" 
-            text="History" 
-            style={{color: '#01233e', padding:'0 1.6rem'}}
-            onClick={()=>{onNavigationTabClick('history')}}
-        />
-        </Navbar.Group>
-    </Navbar>
+    <nav>
+        <Navbar style={{backgroundColor: '#baccaa', color: '#01233e', width:'100%'}}>
+            <Navbar.Group align={Alignment.LEFT}>
+            <Navbar.Heading className='nav-text-style'>
+                <div style={{display:'flex', flexWrap:'nowrap', alignItems:'center'}}>
+                    <span>DeliveryShop</span>
+                    <img src='/images/delivery.png' alt='' style={{width:'36px'}}/>
+                </div>    
+            </Navbar.Heading>
+            <Navbar.Divider />
+            <Button 
+                className="bp4-minimal nav-text-style" 
+                text="Shop" 
+                style={{color: '#01233e', padding:'0 1.6rem'}}
+                onClick={()=>{onNavigationTabClick('shop')}}
+            />
+            <Button 
+                className="bp4-minimal nav-text-style" 
+                text="Shopping Cart" 
+                style={{color: '#01233e', padding:'0 1.6rem'}}
+                onClick={()=>{onNavigationTabClick('cart')}}
+            />
+            <Button 
+                className="bp4-minimal nav-text-style" 
+                text="History" 
+                style={{color: '#01233e', padding:'0 1.6rem'}}
+                onClick={()=>{onNavigationTabClick('history')}}
+            />
+            </Navbar.Group>
+        </Navbar>
+    </nav>    
     );
 }
 
